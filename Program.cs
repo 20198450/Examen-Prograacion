0dfe03e6830bad9bd84791e3eb55002ea663af3e
20198450 <78990916+20198450@users.noreply.github.com>
2021-04-29 18:52:10 -0400

    Proyecto Final (Según lo aprendido)
    Gracias profesor por habernos enseñado a Programar en C#

--- Program.cs
+++ Program.cs
@@ -2,38 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Cargar 
+namespace Consola_Tabla_de_amortizacion
 {
-    class Operarios
+    class Program
     {
-        private int[] sueldos;
-
-        public Operarios()
-        {
-            sueldos = new int[5];
-            for (int f = 0; f < sueldos.Length; f++)
-            {
-                Console.Write("Ingrese el sueldo:");
-                string linea = Console.ReadLine();
-                sueldos[f] = int.Parse(linea);
-            }
-        }
-
-        public void Imprimir()
-        {
-            for (int f = 0; f < sueldos.Length; f++)
-            {
-                Console.WriteLine(sueldos[f]);
-            }
-            Console.ReadKey();
-        }
-
         static void Main(string[] args)
         {
-            Operarios op = new Operarios();
-            op.Imprimir();
+
+            float pago, Interes_pagado, Capital_pagado, Monto_del_prestamo, tasa_interes_anual, tasa_interes_mensual;
+            int fila, Plazo, i;
+
+            Console.Write("INTRODUCE EL MONTO DE TU PRESTAMO: ");
+            float.TryParse(Console.ReadLine(), out Monto_del_prestamo);
+            Console.Write("INTRODUCE LA TASA DE INTERES ANUAL: ");
+            float.TryParse(Console.ReadLine(), out tasa_interes_anual);
+            Console.Write("INTRODUCE EL PLAZO EN MESE: ");
+            int.TryParse(Console.ReadLine(), out Plazo);
+
+            //Calculo del interes mensual
+            tasa_interes_mensual = (tasa_interes_anual / 100) / 12;
+
+
+            //Calculo de la cuota mensual
+            pago = tasa_interes_mensual + 1;
+            pago = (float)Math.Pow(pago, Plazo);
+            pago = pago - 1;
+            pago = tasa_interes_mensual / pago;
+            pago = tasa_interes_mensual + pago;
+            pago = Monto_del_prestamo * pago;
+
+
+
+
+            Console.WriteLine();
+            fila = 1;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" NUMERO DE PAGO \t");
+            Console.Write("PAGO \t\t");
+            Console.Write("INTERES PAGADO \t\t");
+            Console.Write("CAPITAL PAGADO \t\t");
+            Console.Write("MONTO DEL PRESTAMO \t");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("\t0");
+            Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t{0}", Monto_del_prestamo);
+
+
+            for (i = 1; i <= Plazo; i++)
+            {
+
+
+                Console.Write("\t{0}\t\t", fila);
+
+
+                Console.Write("{0}\t", pago);
+
+
+                Interes_pagado = tasa_interes_mensual * Monto_del_prestamo;
+                Console.Write("{0}\t\t", Interes_pagado);
+
+
+                Capital_pagado = pago - Interes_pagado;
+                Console.Write("\t{0}\t", Capital_pagado);
+
+
+                Monto_del_prestamo = Monto_del_prestamo - Capital_pagado;
+                Console.Write("\t{0}\t", Monto_del_prestamo);
+
+                fila = fila + 1;
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
         }
     }
 }
